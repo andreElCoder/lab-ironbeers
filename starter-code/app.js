1f19a2a6b55707a47a0016db214180f9b75bee5f
Andre R <andre.rabaca94@gmail.com>
2020-04-11 16:25:29 +0100

    Bonus missing

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,34 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) =>{
+    punkAPI.getBeers()
+    .then(beersFromApi => {
+        console.log('Beers from the database: ', beersFromApi);
+         const beers = beersFromApi;
+         res.render('beers',{beers})
+    })
+    .catch(error => console.log(error));
+    
+    
+});
+
+app.get('/random-beers',(req,res) => {
+    punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      const beers = responseFromAPI;
+      res.render('random-beers',{beers})
+    })
+    .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
